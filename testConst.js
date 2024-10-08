8e5da20a752d5486efb1106a46e5bf9805997e4e
banciudiana <dianaelenabanciu@gmail.com>
2024-10-08 17:30:18 +0300

    variabile

---
+++ testConst.js
@@ -0,0 +1,12 @@
+let test=3;
+test='3';
+
+console.log(test);
+
+
+const obj={};
+obj.name="Adi";
+console.log(obj.name);
+
+
+
---
+++ var.js
@@ -0,0 +1,20 @@
+function test()
+{
+    if(true)
+    {
+        var var1=1;
+        let let2=2;
+        const const3=3;
+
+    }
+
+    console.log(var1);
+    console.log(let2);
+    console.log(const3);
+}
+
+//test();
+
+console.log(var2);
+
+var var2=2;
